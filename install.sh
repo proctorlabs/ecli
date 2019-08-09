e342efe796e94520dfcc908cbea49af93859cbad
Phil Proctor <philliptproctor@gmail.com>
2019-08-09 08:20:33 -0400

    Separate shebang header into separate file for generate command, another CI test

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+set -Eeou pipefail
+
+echo TODO
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,23 +4,17 @@ use std::io::Write;
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 
-const HEADER: &str = r#"#!/usr/bin/env sh
-# Add .ecli to YAML file associations to get syntax highlighting, or just use a yaml file with
-# the shebang line
-"": | # This hackery is for compatibility with a variety of shebang lines while also keeping the yaml valid
-  true;
-  exec cargo run -- $0
-# "#;
+const HEADER: &str = include_str!("shebang.yml");
 
 pub fn generate(file: PathBuf) -> Result<()> {
-    let config = AppConfig::default();
-    let mut f = std::fs::File::create(file)?;
-    let metadata = f.metadata()?;
-    let mut permissions = metadata.permissions();
-    permissions.set_mode(0o755);
-    f.set_permissions(permissions)?;
+  let config = AppConfig::default();
+  let mut f = std::fs::File::create(file)?;
+  let metadata = f.metadata()?;
+  let mut permissions = metadata.permissions();
+  permissions.set_mode(0o755);
+  f.set_permissions(permissions)?;
 
-    write!(f, "{}", HEADER)?;
-    serde_yaml::to_writer(f, &config)?;
-    Ok(())
+  write!(f, "{}", HEADER)?;
+  serde_yaml::to_writer(f, &config)?;
+  Ok(())
 }
